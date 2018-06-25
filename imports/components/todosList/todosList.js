0dae31ed91a6a939a8aabb9e5e4b1f976ab51451
Angela Sun <angelasun1234@gmail.com>
2018-06-25 17:23:39 +1000

    security feature added + public/private, problem: can't type in new to-do anymore

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -9,8 +9,11 @@ class TodosListCtrl {
   constructor($scope) {
     $scope.viewModel(this);
 
+    this.subscribe('tasks');
+
     this.hideCompleted = false;
 
+    //helper methods for security switch
     this.helpers({
       tasks() {
         const selector = {};
@@ -44,12 +47,7 @@ class TodosListCtrl {
 
   addTask(newTask) {
     // Insert a task into the collection
-    Tasks.insert({
-      text: newTask,
-      createdAt: new Date,
-      owner: Meteor.userId(),
-      username: Meteor.user().username
-    });
+     Meteor.call('tasks.insert', newTask);
 
     // Clear form
     this.newTask = '';
@@ -58,15 +56,15 @@ class TodosListCtrl {
 
   setChecked(task) {
     // Set the checked property to the opposite of its current value
-    Tasks.update(task._id, {
-      $set: {
-        checked: !task.checked
-      },
-    });
+     Meteor.call('tasks.setChecked', task._id, !task.checked);
   }
 
   removeTask(task) {
-    Tasks.remove(task._id);
+    Meteor.call('tasks.remove', task._id);
+  }
+
+  setPrivate(task) {
+    Meteor.call('tasks.setPrivate', task._id, !task.private);
   }
 }
 
